71a3b2b18cf6e32728342b124cf9cccbef1f9ce7
max <max@SIMOCCAUCH29.lan>
2018-09-24 16:13:13 +0200

    sql

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var SerialPort = require('serialport');
 
-
 var port = new SerialPort('/dev/ttyUSB0', 
 {
   baudRate: 9600
@@ -9,13 +8,44 @@ port.on('data', dire_bonjour);
 
 function dire_bonjour( data )
 {
-var say = require('say');
-say.speak('hi luc');
-say.stop();
-       
-// Record();
-// stop();
-// play();
+    var say = require('say');
+    // say.speak('hi marc');
+    // say.stop();
+
+
+	var res=get_speech();
+	say.speak(res);
+	say.stop();
+
+
+	// if (res != null)
+	//  {	
+	//      say.speak(res);
+	//      say.stop();
+	//  }
+
+
 
 }
 
+
+	var texte;
+	var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition ;
+	recognition = new SpeechRecognition();
+	recognition.continuous = true;
+	recognition.lang = "fr-FR";
+	recognition.start();
+
+	recognition.onresult = function (event) {
+		for (i = event.resultIndex; i < event.results.length; i++) 
+		{
+				 texte = textarea.value += event.results[i][0].transcript + "\n"
+				document.getElementById("span").innerHTML =
+					Math.round(event.results[i][0].confidence * 100) + " %";
+		}
+	}
+
+	recognition.onend = function(){
+		say.speak(texte);
+	}
+}
