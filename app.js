cd6cf96b9f1869a2498c0d1321732fabb5843bb5
zakiasegond <segondzakia@live.fr>
2018-09-25 14:03:52 +0200

    synthese vocale operationnelle

--- app.js
+++ app.js
@@ -1,145 +1,121 @@
 
-
 /*
 
 Définitions des variables, appel aux modules 
 de synthèse et reconnaissance vocale 
 
 */
-
+// function speech_to_text
 var synth = window.speechSynthesis;
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
-
 var recognition = new webkitSpeechRecognition();
-        recognition.continuous = false;
-        recognition.interimResults = true;
-        recognition.lang = 'fr-FR';
-        recognition.start();
-        var voice = "";
-        recognition.onresult = function(event){
-         voice = event.results[0][0].transcript;
-        }
-
-        recognition.onend = function(){
-            /*
-            https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
-            */
-            var msg = new SpeechSynthesisUtterance(voice);
-            window.speechSynthesis.speak(msg);
-
-            console.log(msg.text);
-            send(msg.text);
-        }
-function send(test)
+recognition.continuous = false;
+recognition.interimResults = true;
+recognition.lang = 'fr-FR';
+recognition.start();
+var voice = "";
+recognition.onresult = function(event)
 {
-    var visitor_res= test;
-    console.log(visitor_res);
-    $.ajax({
-    method: "POST",
-    url: "/test",
-    data: { visitor_res : visitor_res }
-    }).done(function() {
-    console.log( "success" );
-    }).fail(function() {
-    console.log( "error" );
-    }).always(function() {
-    console.log( "complete" );
-    });
-}
-/*
-
- Fonction Speech-To-Text (reconnaissance vocale)
-
- */
-
-
- // var recognition = new webkitSpeechRecognition();
- // recognition.continuous = false;
- // recognition.interimResults = true;
- // recognition.lang = 'fr-FR';
- //    //recognition.start();
- //    var voice = "";
- //    var your_text = document.getElementById('yourtext').value;
- //    recognition.onresult = function(event)
- //    {
- //    	//voice = event.results[0][0].transcript;
- //    	for (i = event.resultIndex; i < event.results.length; i++)
- //        {
- //            yourtext.value += event.results[i][0].transcript + "\n"
- //        }
- //    }
- //    recognition.onend = function()
- //    {
- //        var msg = new SpeechSynthesisUtterance(voice);
- //        synth.speak(msg);
- //        console.log(msg);
- //    }
-
- //    $('#go').on('click', function ()
- //    {   
- //    recognition.start();
- //    });
-
- 
-
-
-
-
-
-
-
-
-
-/* 
-
-Fonction Text-to-Speech (synthese vocale), prend en paramètre une variable 
-de type string et retourne la transcription orale du message 
-
-*/
-var welcome = document.getElementById('welcome').value;
-function text_to_speech(text){
-    var msg = new SpeechSynthesisUtterance(text);
-    msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == ''; })[0];
-    var result = synth.speak(msg);
-    return result;
-
+   voice = event.results[0][0].transcript;
+   console.log(voice);
 }
 
-
-  
-
-
-
-
-//     recognition.start();
-//     recup();
-
-
-
-// $.get('/test' );
-// console.log(visitor_res);
-
-//document.getElementById("demo").innerHTML = "Hello JavaScript!";
+recognition.onend = function()
+{
+        /*
+        https://developers.google.com/web/updates/2014/01/Web-apps-that-talk-Introduction-to-the-Speech-Synthesis-API
+        */
+        var msg = new SpeechSynthesisUtterance(voice);
+        window.speechSynthesis.speak(msg);
+        console.log(msg.text);
+ }
 
 
-text_to_speech(welcome);
 
 
-/* 
-
-afficher la liste des langages disponibles sur le navigateur 
-
-*/
-
-// speechSynthesis.getVoices().forEach(function(voice) {
-//   console.log(voice.name, voice.default ? voice.default :'');
-// });
+// function text_to_speech
+// var welcome = "Bonjour !. Je m'apelle Arduina, end";
+// //text_to_speech(welcome);
+// function text_to_speech(text)
+// {
+//     var msg = new SpeechSynthesisUtterance(text);
+//     msg.voice = speechSynthesis.getVoices().filter(function(voice) { return voice.name == ''; })[0];
+//     var result = synth.speak(msg);
+//     return result;
+// }
 
 
 
 
 
 
+//CODE D OCEANE..... A ADAPTER A NOTRE CODE
+// function text_to_speech
+$( "#Recommencer" ).click(function()
+ {
+        var msg = new SpeechSynthesisUtterance();
+        msg.lang = 'fr-FR';
+        var synth = window.speechSynthesis;
+        var voices = synth.getVoices();
+
+        speak("Bonjour !. Je m'apelle Arduina,Bienvenu aux portes ouvertes de l'école simplon promo 2, Nous sommes heureux de vous accueillir")
+
+        function speak (message) 
+        {
+        msg.text = message;
+        msg.onend = function () { console.log("on end!"); }
+        msg.onerror = function () { console.log("on error!"); }
+        msg.onpause = function () { console.log("on pause"); }
+        msg.onresume = function () { console.log("on resume"); }
+        msg.onstart = function () { console.log("on start"); }
+        synth.cancel();
+        synth.speak(msg);
+        var r = setInterval(function () {
+        console.log(synth.speaking);
+        if (!synth.speaking) clearInterval(r);
+        else synth.resume();
+        }, 14000);
+
+        msg.onend = function(e) {
+        console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        };
+        }
+ });
+
+$( "#Confirmer" ).click(function()
+ {
+        var msg = new SpeechSynthesisUtterance();
+        msg.lang = 'fr-FR';
+        var synth = window.speechSynthesis;
+        var voices = synth.getVoices();
+    speak("Avez-vous,  2 minutes pour renseigner vos coordonnées s'il vous plaît?")
+    // speak("Etes-vous une femme ou un homme ?")
+    // speak("Quel est votre nom de famille ?")
+    // speak("Quel est votre prénom ?")
+    // speak("Quel est le nom de votre entreprise, organisation ou association ?")
+    // speak("Merci de préciser la ville.")
+    // speak("Où avez-vous entendu parler de cet évènement ?")
+        function speak (message) 
+        {
+        msg.text = message;
+        msg.onend = function () { console.log("on end!"); }
+        msg.onerror = function () { console.log("on error!"); }
+        msg.onpause = function () { console.log("on pause"); }
+        msg.onresume = function () { console.log("on resume"); }
+        msg.onstart = function () { console.log("on start"); }
+        synth.cancel();
+        synth.speak(msg);
+        var r = setInterval(function () {
+        console.log(synth.speaking);
+        if (!synth.speaking) clearInterval(r);
+        else synth.resume();
+        }, 14000);
+
+        msg.onend = function(e) {
+        console.log('Finished in ' + event.elapsedTime + ' seconds.');
+        };
+        }
+ });
 
